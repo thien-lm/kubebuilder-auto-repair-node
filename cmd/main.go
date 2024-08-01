e584fdb1ff1650de7887ed6e506aa0f4c3d7ed29
thien-lm <thien.lm204608@sis.hust.edu.vn>
2024-08-01 17:38:33 +0700

    update

--- cmd/main.go
+++ cmd/main.go
@@ -124,7 +124,7 @@ func main() {
 		WebhookServer:          webhookServer,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "ea72b978.github.com/thien-lm",
+		LeaderElectionID:       "ea72b978.github.com",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
